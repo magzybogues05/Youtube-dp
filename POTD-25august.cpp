3b718ad43964b7ff1f375f83b7e2503063ad37ba
shivam <shivam.gupta452003@gmail.com>
2025-08-25 18:25:53 +0530

    add long long int

--- POTD-25august.cpp
+++ POTD-25august.cpp
@@ -1,43 +1,43 @@
+#define ll long long int
 class Solution {
   public:
-    
-    bool possible(int mid,vector<int>&arr,int k)
+  
+    bool possible(ll mid, vector<int>&arr, int k)
     {
-        int val=0;
+        ll val=0;
         int n=arr.size();
-        int half=n/2;
+        ll half=n/2;
         if(n%2==0)
         {
-            val+=max(0,2*mid-arr[half]-arr[half-1]);
+            val+=max(0LL,2*mid-arr[half]-arr[half-1]);
         }
         else{
-            val+=max(0,mid-arr[half]);
+            val+=max(0LL,mid-arr[half]);
         }
         for(int i=half+1;i<n;i++)
         {
-            val+=max(0,mid-arr[i]);
+            val+=max(0LL,mid-arr[i]);
         }
         return (val<=k);
     }
-  
     int maximizeMedian(vector<int>& arr, int k) {
         // code here
-        sort(arr.begin(),arr.end());
-        int l=0;
-        int r=arr[arr.size()-1]+k;
-        int ans=0;
-        while(l<=r)
-        {
-            int mid=l+(r-l)/2;
-            if(possible(mid,arr,k))
-            {
-                ans=mid;
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
-            }
-        }
-        return ans;
+       sort(arr.begin(), arr.end());
+       ll l=0;
+       ll r=arr[arr.size()-1]+k;
+       ll ans=0;
+       while(l<=r)
+       {
+           ll mid=l+(r-l)/2;
+           if(possible(mid,arr,k))
+           {
+               ans=mid;
+               l=mid+1;
+           }
+           else{
+               r=mid-1;
+           }
+       }
+       return ans;
     }
 };
