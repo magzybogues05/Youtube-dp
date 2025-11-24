cf604ea176f87c305e7f5a2fc956ee2399653925
shivam <shivam.gupta452003@gmail.com>
2025-11-25 03:02:49 +0530

    add POTD-25november

---
+++ POTD-25november.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int subarrayXor(vector<int>& arr) {
+        // code here
+        int ans=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            int times=(i+1)*(n-i);
+            if(times%2!=0)
+            {
+                ans^=arr[i];
+            }
+        }
+        return ans;
+    }
+};
