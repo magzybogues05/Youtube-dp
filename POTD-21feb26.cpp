a92c52baecb625019b99e0c25cae8d91e677acb5
shivam <shivam.gupta452003@gmail.com>
2026-02-21 14:53:22 +0530

    added POTD-21feb26

---
+++ POTD-21feb26.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int hIndex(vector<int>& citations) {
+        // code here
+        int ans = 0;
+        int n = citations.size();
+        sort(citations.begin(),citations.end(),greater<int>());
+        for(int i=0; i<n; i++){
+            if(citations[i]>=(i+1)){
+                ans=i+1;
+            }
+        }
+        return ans;
+    }
+};
