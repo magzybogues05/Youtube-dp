7d098ee4176feb231958a085011f0ad0484f89ef
shivam <shivam.gupta452003@gmail.com>
2026-06-21 07:35:02 +0530

    add 21june26

---
+++ Leetcode-1833.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int mx= *max_element(costs.begin(),costs.end());
+        vector<int> freq(mx+1, 0);
+
+        for (int cost : costs) {
+            freq[cost]++;
+        }
+
+        int count = 0;
+
+        for (int cost = 1; cost <= mx; cost++) 
+        {
+            if(coins < cost) 
+            {
+                break;
+            }
+            if (freq[cost] == 0)
+            {
+                continue;
+            }
+
+            int canBuy = min(freq[cost], coins / cost);
+
+            count += canBuy;
+            coins -= canBuy * cost;
+        }
+
+        return count;
+    }
+};
